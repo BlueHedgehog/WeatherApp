d46589f70eb265c4216838964c982affded82fbe
Leon Winstanley <leon.winstanley2017@my.ntu.ac.uk>
2019-06-13 16:51:11 +0100

    Added new Images

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SettingCardImages();
     }
+
+    public void SettingCardImages(){
+        ImageView image;
+        image = (ImageView) findViewById(R.id.TopCardViewImage);
+        image.setImageResource(R.drawable.cloudyday3);
+    }
+
 }
