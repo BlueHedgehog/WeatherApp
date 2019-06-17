7205e64f1ee313bfe513917381b1ab618e5586e7
Leon Winstanley <leon.winstanley2017@my.ntu.ac.uk>
2019-06-17 16:27:34 +0100

    Added toolbar to all activities

--- app/src/main/java/com/example/weatherapp/SettingsActivity.java
+++ app/src/main/java/com/example/weatherapp/SettingsActivity.java
@@ -2,6 +2,8 @@ package com.example.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 }
